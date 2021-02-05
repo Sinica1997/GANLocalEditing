b056a06828130d375ea4bcd228e4a82132f0c53d
Sinica1997 <38902164+Sinica1997@users.noreply.github.com>
2021-02-05 15:08:02 +0800

    Update spherical_kmeans.py

--- spherical_kmeans.py
+++ spherical_kmeans.py
@@ -8,7 +8,7 @@ from sklearn.utils import check_array, check_random_state
 from sklearn.utils.extmath import row_norms
 import scipy.sparse as sp
 from sklearn.cluster import MiniBatchKMeans
-from sklearn.cluster.kmeans import (
+from sklearn.cluster.kMeans import (
     _init_centroids,
     _labels_inertia,
     _tolerance,
